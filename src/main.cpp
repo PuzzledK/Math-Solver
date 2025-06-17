f6b90680d234cd49b767d4545bfa1c940dac0731
Indra <ikhatri_be22@thapar.edu>
2025-06-17 23:25:00 +0530

    fixed need to run again and again

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,27 @@
 #include <iostream>
 #include "Parser.hpp"
 
-int main() {
-    std::string s;
-    std::getline(std::cin, s);
+int main()
+{
 
-    try {
-        Lexer lexer(s);
-        Parser parser(lexer);
-        auto result = parser.parse();
-        std::cout << "Result: " << result->evaluate() << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+    while (true)
+    {
+        std::string s;
+        std::getline(std::cin, s);
+
+        if(s == "exit") break;
+
+        try
+        {
+            Lexer lexer(s);
+            Parser parser(lexer);
+            auto result = parser.parse();
+            std::cout << "Result: " << result->evaluate() << std::endl;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
     }
 
     return 0;
